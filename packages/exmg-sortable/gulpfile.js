ccfd94473e749c37b4a15a4e0dea1bd0ed35c62c
Mark Smits <mark@exmachina.nl>
2021-11-02 10:53:20 +0100

    add sortable element

---
+++ packages/exmg-sortable/gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const spawn = require('child_process').spawn;
+
+/**
+ * Gulp task to run `tsc --watch` and `polymer serve` in parallel.
+ */
+gulp.task('serve', () => {
+  const spawnOptions = {
+    // `shell` option for Windows compatability. See:
+    // https://nodejs.org/api/child_process.html#child_process_spawning_bat_and_cmd_files_on_windows
+    shell: true,
+    stdio: 'inherit'
+  };
+  spawn('tsc', ['--watch'], spawnOptions);
+  spawn('polymer', ['serve'], spawnOptions);
+});
