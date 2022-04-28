6783b95ef769ad63489760044477eeecbdb13d8f
Rodrigue Couanon <rodrigue@exmachina.nl>
2022-04-28 16:19:56 +0100

    Adds documentation

--- packages/exmg-collapsed/exmg-collapsed.ts
+++ packages/exmg-collapsed/exmg-collapsed.ts
@@ -2,6 +2,9 @@ import {customElement} from 'lit/decorators.js';
 import {ExmgCollapsedBase} from './exmg-collapsed-base.js';
 import {style} from './styles/exmg-collapsed-styles-css.js';
 
+/**
+ * Exmg Collapsed element
+ */
 @customElement('exmg-collapsed')
 export class ExmgCollapsed extends ExmgCollapsedBase {
   static styles = [style];
