1dad620eb67e26ddcef9104f8c2847c4d3e33646
rdrgcnn <rodrigue@exmachina.nl>
2022-09-29 13:48:39 +0100

    Adds a script to create a Jekyll conf to build node_modules in GH pages

---
+++ scripts/docsPublishing.js
@@ -0,0 +1,20 @@
+// requiring path and fs modules
+const path = require('path');
+const fs = require('fs');
+
+const rootPath = path.join(__dirname);
+
+const config = `
+  include: '/node_modules/'
+`;
+
+const main = () => {
+  try {
+    fs.writeFileSync(path.join(rootPath, '_config.yml'), processedIndexFile);
+    console.log('Config for Jekyll successfully created.');
+  } catch (error) {
+    console.log('An error occured: ', error);
+  }
+};
+
+main();
