6563af9719035e04da20696f4d9aaf08f24e6c57
rdrgcnn <rodrigue@exmachina.nl>
2022-09-29 14:35:24 +0100

    Edits script to publish docs

--- scripts/docsPublishing.js
+++ scripts/docsPublishing.js
@@ -5,82 +5,20 @@ const fs = require('fs');
 const rootPath = path.join(__dirname, '../');
 
 const config = `
-# Where things are
-source              : .
-destination         : ./_site
-collections_dir     : .
-plugins_dir         : _plugins # takes an array of strings and loads plugins in that order
-layouts_dir         : _layouts
-data_dir            : _data
-includes_dir        : _includes
-sass:
-  sass_dir: _sass
-collections:
-  posts:
-    output          : true
+title: Exmachina Web Components
+description: >- # this means to ignore newlines until "baseurl:"
+  Exmachina Web Components Documentation and Demo
+domain: exmg.github.io # if you want to force HTTPS, specify the domain without the http at the start, e.g. example.com
+url: https://exmg.github.io # the base hostname and protocol for your site, e.g. http://example.com
+baseurl: /exmachina-web-components/ # place folder name if the site is served in a subfolder
 
-# Handling Reading
-safe                : true
-include             : [".htaccess", "node_modules/"]
-exclude             : ["Gemfile", "Gemfile.lock" "vendor/bundle/", "vendor/cache/", "vendor/gems/", "vendor/ruby/"]
-keep_files          : [".git", ".svn"]
-encoding            : "utf-8"
-markdown_ext        : "markdown,mkdown,mkdn,mkd,md"
-strict_front_matter : false
+# Build settings
+theme: minima
+plugins:
+  - jekyll-feed
 
-# Filtering Content
-show_drafts         : null
-limit_posts         : 0
-future              : false
-unpublished         : false
-
-# Plugins
-whitelist           : []
-plugins             : []
-
-# Conversion
-markdown            : kramdown
-highlighter         : rouge
-lsi                 : false
-excerpt_separator   : "\n\n"
-incremental         : false
-
-# Serving
-detach              : false
-port                : 4000
-host                : 127.0.0.1
-baseurl             : "" 
-show_dir_listing    : false
-
-# Outputting
-permalink           : date
-paginate_path       : /page:num
-timezone            : null
-
-quiet               : false
-verbose             : false
-defaults            : []
-
-liquid:
-  error_mode        : warn
-  strict_filters    : false
-  strict_variables  : false
-  
-gist:
-  noscript: false
-
-# Markdown Processors
-kramdown:
-  syntax_highlighter: rouge
-  math_engine: mathjax
-  auto_ids          : true
-  entity_output     : as_char
-  toc_levels        : [1, 2, 3, 4, 5, 6]
-  smart_quotes      : lsquo,rsquo,ldquo,rdquo
-  input             : GFM
-  hard_wrap         : false
-  footnote_nr       : 1
-  show_warnings     : false
+include:
+  - node_modules/
 `;
 
 const main = () => {
