51e11d014822dc6bf3515362c544a805e769d7f7
KJelles <kevin@exmachina.nl>
2023-01-05 10:38:58 +0100

    wip

--- demo/demos/exmg-markdown-editor/exmg-markdown-editor-demo.ts
+++ demo/demos/exmg-markdown-editor/exmg-markdown-editor-demo.ts
@@ -1,17 +1,55 @@
 import {html, LitElement} from 'lit';
-import {customElement} from 'lit/decorators.js';
+import {customElement, query} from 'lit/decorators.js';
 import '@exmg/exmg-markdown-editor/exmg-markdown-editor.js';
-@customElement('exmg-mardkwon-editor-demo')
-export class DemoSimpleGridTable extends LitElement {
-  protected render() {
+import '@polymer/iron-demo-helpers/demo-snippet.js';
+import 'marked/lib/marked.esm.js';
+
+@customElement('exmg-markdown-editor-demo')
+export class ExmgMarkdownEditorDemo extends LitElement {
+  firstUpdated() {
+    const editor = this.shadowRoot?.querySelector('#test');
+    const md = '#test';
+    editor.markdown = md;
+  }
+  render() {
     return html`
       <div class="centered">
         <ul class="horizontal-section">
-          <li><a href="init.html">Setup editor markdown</a></li>
-          <li><a href="style.html">Fullscreen and Restyled</a></li>
-          <li><a href="toolbar.html">Toolbar</a></li>
-          <li><a href="styling.html">Css styled preview</a></li>
-          <li><a href="custom-renderer.html">Custom renderer</a></li>
+          <demo-snippet>
+            <template>
+              <exmg-markdown-editor id="test" height="300" split-view show-helper-label class="markdown-body">
+              </exmg-markdown-editor>
+            </template>
+          </demo-snippet>
+          <h2>Markdown (label + split-view) unstyled output</h2>
+          <demo-snippet>
+            <template>
+              <exmg-markdown-editor height="300" split-view show-helper-label markdown="# Hello Word!">
+              </exmg-markdown-editor>
+            </template>
+          </demo-snippet>
+          <h2>Markdown (split-view) styled output</h2>
+          <demo-snippet>
+            <template>
+              <exmg-markdown-editor height="300" split-view markdown="# Hello Word!" class="markdown-body">
+              </exmg-markdown-editor>
+            </template>
+          </demo-snippet>
+
+          <h2>Markdown styled output</h2>
+          <demo-snippet>
+            <template>
+              <exmg-markdown-editor height="300" markdown="# Hello Word!" class="markdown-body"> </exmg-markdown-editor>
+            </template>
+          </demo-snippet>
+
+          <h2>Markdown (auto-focus) styled output</h2>
+          <demo-snippet>
+            <template>
+              <exmg-markdown-editor height="300" auto-focus markdown="# Hello Word!" class="markdown-body">
+              </exmg-markdown-editor>
+            </template>
+          </demo-snippet>
         </ul>
       </div>
     `;
