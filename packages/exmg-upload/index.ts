5a347ca0b1ae53ca42097683f178631147e49073
Mark Smits <mark@exmachina.nl>
2023-06-01 14:31:47 +0200

    Added main class export to index

--- packages/exmg-upload/index.ts
+++ packages/exmg-upload/index.ts
@@ -1,3 +1,4 @@
+export {ExmgUpload} from './src/exmg-upload.js';
 export {ExmgUploadBase} from './src/exmg-upload-base.js';
 export {ExmgUploadCrop} from './src/exmg-upload-crop.js';
 export {ExmgUploadCropBase} from './src/exmg-upload-crop-base.js';
@@ -15,5 +16,3 @@ export {style as exmgUploadStyles} from './src/styles/exmg-upload-styles-css.js'
 export {style as exmgUploadDropAreaStyles} from './src/styles/exmg-upload-drop-area-styles-css.js';
 export {style as exmgUploadCropStyles} from './src/styles/exmg-upload-crop-styles-css.js';
 export {style as exmgUploadItemStyles} from './src/styles/exmg-upload-item-styles-css.js';
-
-export * from './src/exmg-upload.js';
