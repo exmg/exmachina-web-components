96f508405ebf6ba8539eae4c3c468c6a98f8b539
KJelles <kevinjelles98@gmail.com>
2023-06-23 16:10:13 +0200

    Added loading state to button cleaned up other ones

--- packages/exmg-button/exmg-button.ts
+++ packages/exmg-button/exmg-button.ts
@@ -1,14 +1,9 @@
-import {html} from 'lit';
-import {property} from 'lit/decorators/property.js';
-import {customElement} from 'lit/decorators/custom-element.js';
-import {eventOptions} from 'lit/decorators/event-options.js';
-
-import {classMap} from 'lit/directives/class-map.js';
-import {ButtonBase} from '@material/mwc-button/mwc-button-base.js';
-import {styles} from '@material/mwc-button/styles.css.js';
-import {style as newStyles} from './styles/exmg-button-styles-css.js';
-import './exmg-spinner.js';
-
+import { html } from 'lit';
+import { property } from 'lit/decorators/property.js';
+import { customElement } from 'lit/decorators/custom-element.js';
+import { MdFilledButton } from '@material/web/button/filled-button.js';
+import '@material/web/circularprogress/circular-progress.js';
+import { style } from './styles/exmg-button-styles-css.js';
 /**
  * exmg-button
  *
@@ -19,99 +14,76 @@ import './exmg-spinner.js';
  * @extends ButtonBase
  */
 @customElement('exmg-button')
-export class ExmgButton extends ButtonBase {
+export class ExmgButton extends MdFilledButton {
   /**
    * Whether or not the button is in loading state
    * @type {Boolean}
    */
-  @property({type: Boolean})
+  @property({ type: Boolean })
   loading = false;
 
   /**
-   * Whether or not the button displays progress
-   * @type {Number}
+   * Whether or not the button is in loading state
+   * @type {Boolean}
    */
-  @property({type: Number})
-  progress?: number;
+  @property({ type: Boolean })
+  elevated = true;
 
-  static styles = [styles, newStyles];
+  /**
+   * Whether or not the button is disabled.
+   */
+  @property({ type: Boolean, reflect: true }) disabled = false;
 
-  protected render() {
-    const classes = {
-      'mdc-button--raised': this.raised,
-      'mdc-button--unelevated': this.unelevated,
-      'mdc-button--outlined': this.outlined,
-      'mdc-button--dense': this.dense,
-    };
-    const loadingClass = {
-      'exmg-loading': this.loading,
-      'exmg-button-content': true,
-    };
-    const mdcButtonIcon = html` <span class="material-icons mdc-button__icon">${this.icon}</span> `;
+  /**
+   * The URL that the link button points to.
+   */
+  @property() href?: string;
+
+  /**
+   * Where to display the linked `href` URL for a link button. Common options
+   * include `_blank` to open in a new tab.
+   */
+  @property() target?: string;
+
+  /**
+   * Whether to render the icon at the inline end of the label rather than the
+   * inline start.
+   *
+   * _Note:_ Link buttons cannot have trailing icons.
+   */
+  @property({ type: Boolean, attribute: 'trailing-icon' }) trailingIcon = false;
+
+  /**
+   * Whether to display the icon or not.
+   */
+  @property({ type: Boolean, attribute: 'has-icon' }) hasIcon = false;
+
+  /**
+   * Whether `preventDefault()` should be called on the underlying button.
+   * Useful for preventing certain native functionalities like preventing form
+   * submissions.
+   */
+  @property({ type: Boolean }) preventClickDefault = false;
+
+  /**
+   * Specifies the type of button, used for controlling forms. When type
+   * is `submit`, the containing form is submitted; when it is `reset` the
+   * form is reset.
+   */
+  @property() type: '' | 'submit' | 'reset' = '';
+
+  static styles = [style];
+
+  protected override render() {
+    // Link buttons may not be disabled
+    const isLoading = this.loading;
+    const isDisabled = this.disabled && !this.href;
+    // Needed for closure conformance
     return html`
-      <button
-        id="button"
-        class="mdc-button ${classMap(classes)}"
-        ?disabled="${this.disabled}"
-        aria-label="${this.label || this.icon}"
-        @focus="${this.handleRippleFocusA}"
-        @blur="${this.handleRippleBlurA}"
-        @mousedown="${this.handleRippleActivateA}"
-        @mouseenter="${this.handleRippleMouseEnterA}"
-        @mouseleave="${this.handleRippleMouseLeaveA}"
-        @touchstart="${this.handleRippleActivateA}"
-        @touchend="${this.handleRippleDeactivateA}"
-        @touchcancel="${this.handleRippleDeactivateA}"
-      >
-        ${this.renderRipple()}
-        <span class="${classMap(loadingClass)}">
-          ${this.icon && !this.trailingIcon ? mdcButtonIcon : ''}
-          <span class="mdc-button__label">${this.label}</span>
-          ${this.icon && this.trailingIcon ? mdcButtonIcon : ''}
-          <slot></slot>
-        </span>
-        ${this.loading ? html` <exmg-button-spinner active></exmg-button-spinner> ` : ''}
-        ${this.progress
-          ? html`
-              <div class="progress-holder">
-                <progress max="100" value=${this.progress}></progress>
-              </div>
-            `
-          : ''}
-      </button>
+      <md-filled-button ?disabled=${isDisabled}>
+        ${isLoading ? html` <md-circular-progress indeterminate></md-circular-progress>` : html`<slot></slot>`}
+      </md-filled-button>
     `;
-  }
-
-  @eventOptions({passive: true})
-  private handleRippleActivateA(evt?: Event) {
-    const onUp = () => {
-      window.removeEventListener('mouseup', onUp);
-
-      this.handleRippleDeactivateA();
-    };
-
-    window.addEventListener('mouseup', onUp);
-    this.rippleHandlers.startPress(evt);
-  }
-
-  private handleRippleDeactivateA() {
-    this.rippleHandlers.endPress();
-  }
-
-  private handleRippleMouseEnterA() {
-    this.rippleHandlers.startHover();
-  }
-
-  private handleRippleMouseLeaveA() {
-    this.rippleHandlers.endHover();
-  }
-
-  private handleRippleFocusA() {
-    this.rippleHandlers.startFocus();
-  }
-
-  private handleRippleBlurA() {
-    this.rippleHandlers.endFocus();
   }
 }
 
