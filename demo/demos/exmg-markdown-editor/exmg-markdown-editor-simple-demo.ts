55abe97b825be5fb0d64a79729e3b8d987e470f6
rdrgcnn <rodrigue@exmachina.nl>
2024-01-04 10:40:20 +0100

    Resolves Mark comments

--- demo/demos/exmg-markdown-editor/exmg-markdown-editor-simple-demo.ts
+++ demo/demos/exmg-markdown-editor/exmg-markdown-editor-simple-demo.ts
@@ -2,8 +2,6 @@ import { html, css, LitElement } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { markdown } from './markdown.js';
 import '@exmg/exmg-markdown-editor/src/exmg-markdown-editor.js';
-import '@material/web/textfield/filled-text-field.js';
-import '../../src/theme/color-pallet.js';
 
 @customElement('exmg-markdown-editor-simple-demo')
 export class ExmgMarkdownEditorSimpleDemo extends LitElement {
@@ -16,7 +14,7 @@ export class ExmgMarkdownEditorSimpleDemo extends LitElement {
   ];
 
   render() {
-    return html` <exmg-markdown-editor label="Exmg Markdown Demo" markdown=${markdown}></exmg-markdown-editor> <br /> <md-filled-text-field type="textarea" label="Text Label"></md-filled-text-field> <br/> <color-pallet></color-pallet>`;
+    return html` <exmg-markdown-editor label="Exmg Markdown Demo" markdown=${markdown}></exmg-markdown-editor>`;
   }
 }
 
