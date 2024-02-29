e510287b0f134ce7c582d35b88e3d1241cf9b2f1
Mark Smits <mark@exmachina.nl>
2024-02-29 10:53:21 +0100

    Fix for drawer height issue

--- demo/src/elements.ts
+++ demo/src/elements.ts
@@ -8,75 +8,75 @@
   export const elements: Element[] = [
     {
       name: '@exmg/exmg-breadcrumbs',
-      version: '9.0.1',
+      version: '9.0.4',
       url: 'https://www.npmjs.com/package/@exmg/exmg-breadcrumbs',
     },{
       name: '@exmg/exmg-button',
-      version: '9.0.1',
+      version: '9.0.4',
       url: 'https://www.npmjs.com/package/@exmg/exmg-button',
     },{
       name: '@exmg/exmg-chip-input',
-      version: '9.0.1',
+      version: '9.0.3',
       url: 'https://www.npmjs.com/package/@exmg/exmg-chip-input',
     },{
       name: '@exmg/exmg-collapsed',
-      version: '9.0.1',
+      version: '9.0.4',
       url: 'https://www.npmjs.com/package/@exmg/exmg-collapsed',
     },{
       name: '@exmg/exmg-copy-to-clipboard',
-      version: '9.0.1',
+      version: '9.0.4',
       url: 'https://www.npmjs.com/package/@exmg/exmg-copy-to-clipboard',
     },{
       name: '@exmg/exmg-dialogs',
-      version: '9.0.1',
+      version: '9.0.4',
       url: 'https://www.npmjs.com/package/@exmg/exmg-dialogs',
     },{
       name: '@exmg/exmg-form',
-      version: '9.0.1',
+      version: '9.0.4',
       url: 'https://www.npmjs.com/package/@exmg/exmg-form',
     },{
       name: '@exmg/exmg-form-drawer',
-      version: '9.0.1',
+      version: '9.0.4',
       url: 'https://www.npmjs.com/package/@exmg/exmg-form-drawer',
     },{
       name: '@exmg/exmg-grid',
-      version: '9.0.1',
+      version: '9.0.4',
       url: 'https://www.npmjs.com/package/@exmg/exmg-grid',
     },{
       name: '@exmg/exmg-markdown-editor',
-      version: '9.0.1',
+      version: '9.0.4',
       url: 'https://www.npmjs.com/package/@exmg/exmg-markdown-editor',
     },{
       name: '@exmg/exmg-navigation',
-      version: '9.0.1',
+      version: '9.0.4',
       url: 'https://www.npmjs.com/package/@exmg/exmg-navigation',
     },{
       name: '@exmg/exmg-radio',
-      version: '9.0.0',
+      version: '9.0.4',
       url: 'https://www.npmjs.com/package/@exmg/exmg-radio',
     },{
       name: '@exmg/exmg-search',
-      version: '9.0.1',
+      version: '9.0.3',
       url: 'https://www.npmjs.com/package/@exmg/exmg-search',
     },{
       name: '@exmg/exmg-snackbar',
-      version: '9.0.1',
+      version: '9.0.3',
       url: 'https://www.npmjs.com/package/@exmg/exmg-snackbar',
     },{
       name: '@exmg/exmg-sortable',
-      version: '9.0.1',
+      version: '9.0.4',
       url: 'https://www.npmjs.com/package/@exmg/exmg-sortable',
     },{
       name: '@exmg/exmg-theme',
-      version: '9.0.1',
+      version: '9.0.4',
       url: 'https://www.npmjs.com/package/@exmg/exmg-theme',
     },{
       name: '@exmg/exmg-tooltip',
-      version: '9.0.1',
+      version: '9.0.3',
       url: 'https://www.npmjs.com/package/@exmg/exmg-tooltip',
     },{
       name: '@exmg/exmg-upload',
-      version: '9.0.1',
+      version: '9.0.4',
       url: 'https://www.npmjs.com/package/@exmg/exmg-upload',
     },
   ];
